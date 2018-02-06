e7cf8dd1f8d0daeb7ae20f5a2c4c670345742958
xjhnear <xiajiahui@jucaicat.com>
2018-02-06 17:55:10 +0800

    fix
    Signed-off-by: xjhnear <xiajiahui@jucaicat.com>

--- backend/config/main-local.php
+++ backend/config/main-local.php
@@ -9,25 +9,18 @@ $config = [
     ],
 ];
 
-if (!YII_ENV_PROD) {
+if (YII_ENV_TEST) {
     // configuration adjustments for 'dev' environment
     $config['bootstrap'][] = 'debug';
     $config['modules']['debug'] = [
         'class' => 'yii\debug\Module',
-        'allowedIPs' => ['127.0.0.1', '::1', '47.100.101.44']
     ];
 
     $config['bootstrap'][] = 'gii';
     $config['modules']['gii'] = [
         'class' => 'yii\gii\Module',
-        'allowedIPs' => ['127.0.0.1', '::1', '47.100.101.44']
     ];
-} else {
-    $config['bootstrap'][] = 'gii';
-    $config['modules']['gii'] = [
-        'class' => 'yii\gii\Module',
-        'allowedIPs' => ['127.0.0.1', '::1', '*.*.*.*']
-    ];
+
 }
 
 return $config;
