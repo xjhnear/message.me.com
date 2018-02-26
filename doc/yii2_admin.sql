03c6908c1f3e0b520147ad5f777ca63c8956cdf8
Near <250327864@qq.com>
2018-02-26 16:49:35 +0800

    fix
    Signed-off-by: Near <250327864@qq.com>

--- doc/yii2_admin.sql
+++ doc/yii2_admin.sql
@@ -10,13 +10,13 @@ Target Server Type    : MYSQL
 Target Server Version : 50720
 File Encoding         : 65001
 
-Date: 2018-02-24 09:56:43
+Date: 2018-02-26 15:55:37
 */
 
 SET FOREIGN_KEY_CHECKS=0;
 
 -- ----------------------------
--- Table structure for yii2_admin
+-- Table structure for `yii2_admin`
 -- ----------------------------
 DROP TABLE IF EXISTS `yii2_admin`;
 CREATE TABLE `yii2_admin` (
@@ -43,12 +43,13 @@ CREATE TABLE `yii2_admin` (
   UNIQUE KEY `username` (`username`),
   UNIQUE KEY `email` (`email`),
   KEY `status` (`status`)
-) ENGINE=MyISAM AUTO_INCREMENT=11 DEFAULT CHARSET=utf8 COMMENT='用户表';
+) ENGINE=MyISAM AUTO_INCREMENT=12 DEFAULT CHARSET=utf8 COMMENT='用户表';
 
 -- ----------------------------
 -- Records of yii2_admin
 -- ----------------------------
 INSERT INTO `yii2_admin` VALUES ('1', 'administrator', '$2y$13$0UVcG.mXF6Og0rnjfwJd2.wixT2gdn.wDO9rN44jGtIGc6JvBqR7i', 'SbSY36BLw3V2lU-GB7ZAzCVJKDFx82IJ', 'phphome111@qq.com', '13565231112', null, null, null, '1457922160', '2130706433', '1457922174', '2130706433', '1518148691', '1', '0', '0', '1', '0');
-INSERT INTO `yii2_admin` VALUES ('8', 'test', '$2y$13$A94adm0j9A.74StXOCwh3uswtkEUj6U1KImZWRagCOv89RGohmITW', 'eiUh5Y4xi6udtXKXIMDAchC156MzlGxy', 'test@qq.com', '13012345678', null, null, null, '1517365306', '2130706433', '0', '2130706433', '1518419513', '1', '0', '1', '1', '8994');
+INSERT INTO `yii2_admin` VALUES ('8', 'test', '$2y$13$A94adm0j9A.74StXOCwh3uswtkEUj6U1KImZWRagCOv89RGohmITW', 'eiUh5Y4xi6udtXKXIMDAchC156MzlGxy', 'test@qq.com', '13012345678', null, null, null, '1517365306', '2130706433', '0', '2130706433', '1518419513', '1', '0', '1', '1', '11091');
 INSERT INTO `yii2_admin` VALUES ('9', 'check', '$2y$13$7Ek.ZYSCOm7YFuyqQBBVh.nKdLLxcS7NV8Gp8fa1H99LBJKud44Ha', '9MCQIl1cXEdlTzpkE9QJNeKvkfYId5C2', 'check@qq.com', '13612345678', null, null, null, '1518148628', '2130706433', '0', '2130706433', '0', '1', '0', '2', '1', '0');
 INSERT INTO `yii2_admin` VALUES ('10', 'admin', '$2y$13$0jWU1O3qVhJJ5sQ.iC2svuE6.X35zF3Gn/ePTIOnsX1bLjggX0RzK', 'p-2MXy2ujzJP3vxCAj35HzEokWil0o3m', 'admin@qq.com', '15812345678', null, null, null, '1518148730', '2130706433', '0', '2130706433', '0', '1', '0', '3', '1', '0');
+INSERT INTO `yii2_admin` VALUES ('11', 'jiehong', '$2y$13$e8LD0CApOMvVRx20pY3sZ.NzCaYH8KqI90Tvumb7HCI.GevzwXLrq', 'C8BbmASmrShtki_v9skoBxHXvDwAN9M9', '20870835@qq.com', '13916058395', '', '', '', '1519610328', '-1263680645', '0', '2130706433', '1519610350', '1', '0', '1', '0.05', '10000');
