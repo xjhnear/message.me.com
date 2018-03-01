64b0e2cb83d62c9bd1131dffd1ddc0c8c17a7de7
xjhnear <xiajiahui@jucaicat.com>
2018-03-01 18:00:39 +0800

    fix
    Signed-off-by: xjhnear <xiajiahui@jucaicat.com>

--- doc/yii2_admin.sql
+++ doc/yii2_admin.sql
@@ -10,13 +10,13 @@ Target Server Type    : MYSQL
 Target Server Version : 50720
 File Encoding         : 65001
 
-Date: 2018-02-26 15:55:37
+Date: 2018-03-01 17:52:28
 */
 
 SET FOREIGN_KEY_CHECKS=0;
 
 -- ----------------------------
--- Table structure for `yii2_admin`
+-- Table structure for yii2_admin
 -- ----------------------------
 DROP TABLE IF EXISTS `yii2_admin`;
 CREATE TABLE `yii2_admin` (
@@ -37,19 +37,20 @@ CREATE TABLE `yii2_admin` (
   `status` tinyint(4) DEFAULT '0' COMMENT '用户状态 1正常 0禁用',
   `is_del` tinyint(4) DEFAULT '0' COMMENT '删除状态 1已删除 0正常',
   `role` tinyint(4) DEFAULT '0' COMMENT '角色 1-商户 2-审核员 3-管理员 0-其他',
-  `coefficient` varchar(10) DEFAULT '1' COMMENT '单价',
+  `coefficient` float(10,3) DEFAULT '0.010' COMMENT '单价',
   `balance` int(60) DEFAULT '0' COMMENT '账户余额',
   PRIMARY KEY (`uid`),
   UNIQUE KEY `username` (`username`),
   UNIQUE KEY `email` (`email`),
   KEY `status` (`status`)
-) ENGINE=MyISAM AUTO_INCREMENT=12 DEFAULT CHARSET=utf8 COMMENT='用户表';
+) ENGINE=MyISAM AUTO_INCREMENT=21 DEFAULT CHARSET=utf8 COMMENT='用户表';
 
 -- ----------------------------
 -- Records of yii2_admin
 -- ----------------------------
-INSERT INTO `yii2_admin` VALUES ('1', 'administrator', '$2y$13$0UVcG.mXF6Og0rnjfwJd2.wixT2gdn.wDO9rN44jGtIGc6JvBqR7i', 'SbSY36BLw3V2lU-GB7ZAzCVJKDFx82IJ', 'phphome111@qq.com', '13565231112', null, null, null, '1457922160', '2130706433', '1457922174', '2130706433', '1518148691', '1', '0', '0', '1', '0');
-INSERT INTO `yii2_admin` VALUES ('8', 'test', '$2y$13$A94adm0j9A.74StXOCwh3uswtkEUj6U1KImZWRagCOv89RGohmITW', 'eiUh5Y4xi6udtXKXIMDAchC156MzlGxy', 'test@qq.com', '13012345678', null, null, null, '1517365306', '2130706433', '0', '2130706433', '1518419513', '1', '0', '1', '1', '11091');
-INSERT INTO `yii2_admin` VALUES ('9', 'check', '$2y$13$7Ek.ZYSCOm7YFuyqQBBVh.nKdLLxcS7NV8Gp8fa1H99LBJKud44Ha', '9MCQIl1cXEdlTzpkE9QJNeKvkfYId5C2', 'check@qq.com', '13612345678', null, null, null, '1518148628', '2130706433', '0', '2130706433', '0', '1', '0', '2', '1', '0');
-INSERT INTO `yii2_admin` VALUES ('10', 'admin', '$2y$13$0jWU1O3qVhJJ5sQ.iC2svuE6.X35zF3Gn/ePTIOnsX1bLjggX0RzK', 'p-2MXy2ujzJP3vxCAj35HzEokWil0o3m', 'admin@qq.com', '15812345678', null, null, null, '1518148730', '2130706433', '0', '2130706433', '0', '1', '0', '3', '1', '0');
-INSERT INTO `yii2_admin` VALUES ('11', 'jiehong', '$2y$13$e8LD0CApOMvVRx20pY3sZ.NzCaYH8KqI90Tvumb7HCI.GevzwXLrq', 'C8BbmASmrShtki_v9skoBxHXvDwAN9M9', '20870835@qq.com', '13916058395', '', '', '', '1519610328', '-1263680645', '0', '2130706433', '1519610350', '1', '0', '1', '0.05', '10000');
+INSERT INTO `yii2_admin` VALUES ('1', 'administrator', '$2y$13$0UVcG.mXF6Og0rnjfwJd2.wixT2gdn.wDO9rN44jGtIGc6JvBqR7i', 'SbSY36BLw3V2lU-GB7ZAzCVJKDFx82IJ', 'phphome111@qq.com', '13565231112', null, null, null, '1457922160', '2130706433', '1457922174', '2130706433', '1518148691', '1', '0', '0', '1.000', '0');
+INSERT INTO `yii2_admin` VALUES ('8', 'test', '$2y$13$A94adm0j9A.74StXOCwh3uswtkEUj6U1KImZWRagCOv89RGohmITW', 'eiUh5Y4xi6udtXKXIMDAchC156MzlGxy', 'test@qq.com', '13012345678', null, null, null, '1517365306', '2130706433', '0', '2130706433', '1518419513', '1', '0', '1', '1.000', '11086');
+INSERT INTO `yii2_admin` VALUES ('9', 'check', '$2y$13$7Ek.ZYSCOm7YFuyqQBBVh.nKdLLxcS7NV8Gp8fa1H99LBJKud44Ha', '9MCQIl1cXEdlTzpkE9QJNeKvkfYId5C2', 'check@qq.com', '13612345678', null, null, null, '1518148628', '2130706433', '0', '2130706433', '0', '1', '0', '2', '1.000', '0');
+INSERT INTO `yii2_admin` VALUES ('10', 'admin', '$2y$13$0jWU1O3qVhJJ5sQ.iC2svuE6.X35zF3Gn/ePTIOnsX1bLjggX0RzK', 'p-2MXy2ujzJP3vxCAj35HzEokWil0o3m', 'admin@qq.com', '15812345678', null, null, null, '1518148730', '2130706433', '0', '2130706433', '0', '1', '0', '3', '1.000', '0');
+INSERT INTO `yii2_admin` VALUES ('11', 'jiehong', '$2y$13$e8LD0CApOMvVRx20pY3sZ.NzCaYH8KqI90Tvumb7HCI.GevzwXLrq', 'C8BbmASmrShtki_v9skoBxHXvDwAN9M9', '20870835@qq.com', '13916058395', '', '', '', '1519610328', '-1263680645', '0', '2130706433', '1519610350', '1', '0', '1', '0.050', '10026');
+INSERT INTO `yii2_admin` VALUES ('20', 'zhangxiang', '$2y$13$dFPyq.1ct/KlAuxkOqXUNuDFwTOreVlp64LHf1nmzJzEBrGmFjKl6', 'vZ8xPKh6zHKMcaVy0OtG-dedujxnmadY', '222@qq.com', '13916058395', '', '', '', '1519895736', '-1263680645', '0', '2130706433', '0', '1', '0', '1', '0.010', '0');
--- doc/yii2_channel.sql
+++ doc/yii2_channel.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50720
 File Encoding         : 65001
 
-Date: 2018-02-24 18:30:06
+Date: 2018-03-01 14:50:21
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -29,15 +29,17 @@ CREATE TABLE `yii2_channel` (
   `operator` int(1) DEFAULT '4' COMMENT '运营商 1-联通 2-移动 3-电信 4-其他',
   `status` tinyint(4) DEFAULT '0' COMMENT '通道状态 1正常 0禁用',
   `is_del` tinyint(4) DEFAULT '0' COMMENT '删除状态 1已删除 0正常',
+  `type` tinyint(4) DEFAULT '1' COMMENT '渠道类型 1-普通版 2-签名版',
   PRIMARY KEY (`channel_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4;
+) ENGINE=MyISAM AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4;
 
 -- ----------------------------
 -- Records of yii2_channel
 -- ----------------------------
-INSERT INTO `yii2_channel` VALUES ('1', '天阳短信--联通', '1081', '杰鸿联通贷款', 'a1234561', '', '1', '1', '0');
-INSERT INTO `yii2_channel` VALUES ('2', '天阳短信--移动', '8710', '借鸿移动贷款', 'a123456', '', '2', '1', '0');
-INSERT INTO `yii2_channel` VALUES ('3', '天阳短信--电信', '8710', '借鸿移动贷款', 'a123456', '', '3', '1', '0');
-INSERT INTO `yii2_channel` VALUES ('4', '天阳短信-商超-移动', '1080', '杰鸿三网营销', 'a123456', '', '2', '1', '0');
-INSERT INTO `yii2_channel` VALUES ('5', '天阳短信-商超-联通', '1080', '天阳短信-商超-移动', 'a123456', '', '1', '1', '0');
-INSERT INTO `yii2_channel` VALUES ('6', '天阳短信-商超-电信', '1080', '杰鸿三网营销', 'a123456', '', '3', '1', '0');
+INSERT INTO `yii2_channel` VALUES ('1', '天阳短信--联通', '1081', '杰鸿联通贷款', 'a123456', 'http://139.224.197.119:5588', '1', '1', '0', '1');
+INSERT INTO `yii2_channel` VALUES ('2', '天阳短信--移动', '8710', '借鸿移动贷款', 'a123456', 'http://139.196.58.248:5577', '2', '1', '0', '1');
+INSERT INTO `yii2_channel` VALUES ('3', '天阳短信--电信', '8710', '借鸿移动贷款', 'a123456', 'http://139.196.58.248:5577', '3', '1', '0', '1');
+INSERT INTO `yii2_channel` VALUES ('4', '天阳短信-商超-移动', '1080', '杰鸿三网营销', 'a123456', 'http://139.224.197.119:5588', '2', '1', '0', '1');
+INSERT INTO `yii2_channel` VALUES ('5', '天阳短信-商超-联通', '1080', '天阳短信-商超-移动', 'a123456', 'http://139.224.197.119:5588', '1', '1', '0', '1');
+INSERT INTO `yii2_channel` VALUES ('6', 'http://139.224.197.119:5588', '1080', '杰鸿三网营销', 'a123456', 'http://139.196.58.248:5577', '3', '1', '0', '1');
+INSERT INTO `yii2_channel` VALUES ('7', 'test', '1323', 'test', 'test', 'test', '1', '1', '1', '1');
